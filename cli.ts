b97e46a4cc3462e89a74623c87368148ae959415
Yoshiya Hinosawa <stibium121@gmail.com>
2021-06-03 19:16:15 +0900

    chore: bump to v0.1.0

--- cli.ts
+++ cli.ts
@@ -7,7 +7,7 @@ import { generate } from "./mod.ts";
 import debounce from "https://esm.sh/debounce@1.2.1";
 
 const NAME = "twd";
-const VERSION = "0.0.1";
+const VERSION = "0.1.0";
 
 function usage() {
   console.log(`
