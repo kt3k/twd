d998963c19e5b880fa53a632ebaed115ee31bc01
Yoshiya Hinosawa <stibium121@gmail.com>
2021-07-13 11:51:34 +0900

    chore: bump to v0.4.5

--- cli.ts
+++ cli.ts
@@ -11,7 +11,7 @@ import { Config } from "./types.ts";
 import debounce from "https://esm.sh/debounce@1.2.1";
 
 const NAME = "twd";
-const VERSION = "0.4.4";
+const VERSION = "0.4.5";
 
 function usage() {
   console.log(`
@@ -80,7 +80,7 @@ export async function main(cliArgs: string[]): Promise<number> {
         await Deno.writeTextFile(
           "twd.ts",
           `
-import { Config } from "https://deno.land/x/twd@v0.4.4/types.ts";
+import { Config } from "https://deno.land/x/twd@v0.4.5/types.ts";
 
 export const config: Config = {
   preflight: true,
