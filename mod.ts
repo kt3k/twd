7178644dd8adae29cf700dc0677c33f331546165
Satya Rohith <me@satyarohith.com>
2021-08-10 15:01:19 +0530

    fix: special case hasOnwProperty

--- mod.ts
+++ mod.ts
@@ -37,7 +37,7 @@ export function generate(docs: string[], { tw, sheet }: TwInfo): string {
     if (m) {
       for (const c of m) {
         // See https://github.com/tw-in-js/twind/issues/189
-        if (c === "toLocaleString" || c === "__defineGetter__") {
+        if (c === "toLocaleString" || c === "__defineGetter__" || c === "hasOwnProperty") {
           continue;
         }
         try {
